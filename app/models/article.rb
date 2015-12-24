5d7302db7cfc1ff4bb54e72b505c7c7707369f1d
Haaruun <haaruun.dhubat@outlook.com>
2015-12-24 00:54:11 -0500

    fff

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,6 @@ class Article < ActiveRecord::Base
   has_attached_file :image, styles: {large: "800x800", medium: "300x300", thumb: "150x150"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
-
 	belongs_to :category
 
 	has_many :comments
