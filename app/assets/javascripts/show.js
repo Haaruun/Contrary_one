91c374e5760b39dfdf59ae1a362338b0eb8d9e44
Haaruun <haaruun.dhubat@outlook.com>
2015-12-24 03:50:23 -0500

    fdd

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -14,4 +14,18 @@ $('.modalClose').click(function(){
 	modal.close();
     $('dialog').removeClass('see'); 
 });
+
+
+$(window).scroll(function(){
+
+
+	var scroll = $(this).scrollTop();
+
+
+	$('.blos').css({'transform': 'translate(0,'+ scroll/2.5 +'px)'});
+
+	$('.head_show').css({'-webkit-filter': 'grayscale(' + scroll/3 + '%'});
+    
+
+});    
 });
